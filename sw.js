a3b1d7cdd75a6d7e3d359a14cfb8234bd442e455
TheArchitectOnline <ashafkhan051015@gmail.com>
2026-09-11 12:16:27 +1200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="arc-v2-29-live";
+const CACHE="arc-v2-30-live";
 const OLD_PREFIX="arc-v2-";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(OLD_PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
